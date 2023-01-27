d5c01dddf486fe132e96e7f2c75b7009edbefb8f
Nikita Goncarenko <69197950+nikeedev@users.noreply.github.com>
2023-01-27 07:58:14 +0100

    Update main.v

--- src/main.v
+++ src/main.v
@@ -21,14 +21,14 @@ fn main() {
 	}
 
 	conf.header.add_custom('X-Requested-With', 'XMLHttpRequest') !
-    conf.header.add_custom('X-CSRFToken', 'a') !
+        conf.header.add_custom('X-CSRFToken', 'a') !
 	conf.header.add_custom('Referer', 'https://scratch.mit.edu') !
 	conf.header.add_custom('Cookie', 'scratchcsrftoken=a;') !
 	conf.header.add_custom('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36') !
 	conf.header.add_custom('content_type', 'application/json') !
 
-	mut response := http.fetch(conf) !
-
+	// mut response := http.fetch(conf) !
+	mut response := http.post_json('https://scratch.mit.edu/login/', json.encode(user)) !
 	// time.sleep(1500)
 
 	println(response.header.get_custom('scratchsessionid', exact: true)!)
